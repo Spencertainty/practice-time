4f68b6adaf480a42c38ab5840682332f46444391
Spencertainty <sjmfeagin@gmail.com>
2024-03-01 10:34:56 -0500

    updated to number guessing game

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-document.addEventListener("DOMContentLoaded", () => {
-    function createParagraph() {
-      const para = document.createElement("p");
-      para.textContent = "You clicked the button!";
-      document.body.appendChild(para);
-    }
-  
-    const buttons = document.querySelectorAll("button");
-  
-    for (const button of buttons) {
-      button.addEventListener("click", createParagraph);
-    }
-  });
+let randomNumber = Math.floor(Math.random() * 100) + 1;
+
+const guesses = document.querySelector(".guesses");
+const lastResult = document.querySelector(".lastResult");
+const lowOrHi = document.querySelector(".lowOrHi");
+
+const guessSubmit = document.querySelector(".guessSubmit");
+const guessField = document.querySelector(".guessField");
+
+let guessCount = 1;
+let resetButton;
+
+function checkGuess() {
+  alert("I am a placeholder");
+}
